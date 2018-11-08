5fba3d02865eeab0d0ec45b05bd84e89d9b22911
biaoge <365056996@qq.com>
2018-11-08 16:02:00 +0800

    短暂的胜利继续努力

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,15 +3,16 @@
 @section('content')
 
 <div class="container-fluid">
-    <div class="side-body padding-top text-center">
-      {{-- 局部页面中的局部页面引用方式 --}}
-
+    <div class="side-body padding-top">     
+        {{-- 个人中心页面内容 --}}
+        <div class= "row  text-center">
+          {{-- 局部页面中的局部页面引用方式 --}}
           <section class="user_info">
             {{-- {{ $user->name }} - {{ $user->email }} --}}
             @include('shared._user_info', ['user' => $user])
             {{-- 通过给 @include 方法传参，将用户数据以关联数组的形式传送到 _user_info 局部视图上。 --}}
-          </section>
-        
+          </section>    
+        </div>
     </div>
 </div>
     
